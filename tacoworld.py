bda531bf7439a51dba584727e27c0ce7ff6705b6
dl1069 <70745364+dl1069@users.noreply.github.com>
2020-11-04 20:39:37 +0800

    Revert "Pipeline"

---
+++ tacoworld.py
@@ -0,0 +1,5 @@
+def print_line():
+    print("Today, actually, I (Zach Lamberty), want OTR!")
+    print("I, Eamon Lamberty, want tacos, so it's good there are enough to share!")
+if __name__ == '__main__':
+    print_line()
